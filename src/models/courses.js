d7cf31a8aee8d9983b324c3d51675e1c6d370e8b
Chris Gaona <thebeloved88@sbcglobal.net>
2016-07-17 14:14:49 -0700

    Updates the Course schema with an overallRating virtual property

--- src/models/courses.js
+++ src/models/courses.js
@@ -32,6 +32,17 @@ var CourseSchema = new mongoose.Schema({
   }]
 });
 
+// Update the Course schema with an overallRating virtual property.
+// overallRating is a calculated, read only property that returns the average of all of the review ratings for this course rounded to the nearest whole number.
+// By not storing this calculated value in the database, we ensure that it's impossible for the value to get out of sync with the course's reviews.
+CourseSchema.virtual('reviews.overallRating').get(function () {
+  var total = 0;
+  for (var i = 0; i < this.reviews.length; i++) {
+    total += this.reviews[i];
+  }
+  return total / this.reviews.length;
+});
+
 var Course = mongoose.model('Course', CourseSchema);
 
 module.exports = Course;

b2d29177448f0693ea7170a6459915f225567da7
Chris Gaona <thebeloved88@sbcglobal.net>
2016-07-16 20:52:22 -0700

    Stubs out the required REST API routes

--- src/index.js
+++ src/index.js
@@ -3,19 +3,25 @@
 // load modules
 var express = require('express');
 var morgan = require('morgan');
+var jsonParser = require('body-parser').json;
 
 var app = express();
+
+var routes = require('./routes.js');
 
 // set our port
 app.set('port', process.env.PORT || 5000);
 
 // morgan gives us http request logging
 app.use(morgan('dev'));
+// used to parse json
+app.use(jsonParser());
 
 // setup our static route to serve files from the "public" folder
 app.use('/', express.static('public'));
+app.use('/api', routes);
 
 // start listening on our port
 var server = app.listen(app.get('port'), function() {
-  console.log('Express server is listening on port ' + server.address().port);  
+  console.log('Express server is listening on port ' + server.address().port);
 });
---
+++ src/routes.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+//GET /api/courses 200 - Returns the Course "_id" and "title" properties
+router.get('/courses', function (req, res, next) {
+  res.json('You sent a GET request to return all courses');
+});
+
+// GET /api/courses/:id 200 - Returns all Course properties and related documents for the provided course ID
+router.get('/courses/:id', function (req, res, next) {
+  res.json('You sent a GET request for a specific course: ' + req.params.id);
+});
+
+// POST /api/courses 201 - Creates a course, sets the Location header, and returns no content
+router.post('/courses', function (req, res, next) {
+  res.status(201).json('You sent a POST request to create a new course');
+});
+
+// PUT /api/courses/:id 204 - Updates a course and returns no content
+router.put('/courses/:id', function (req, res, next) {
+  res.status(204).json('You sent a PUT request to update a course: ' + req.params.id);
+});
+
+// GET /api/users 200 - Returns the currently authenticated user
+router.get('/users', function (req, res, next) {
+  res.json('You sent a GET request to find the current user');
+});
+
+// POST /api/users 201 - Creates a user, sets the Location header to "/", and returns no content
+router.post('/users', function (req, res, next) {
+  res.status(201).json('You sent a POST request to create a new user');
+});
+
+// POST /api/courses/:courseId/reviews 201 - Creates a review for the specified course ID, sets the Location header to the related course, and returns no content
+router.post('/courses/:courseId/reviews', function (req, res, next) {
+  res.status(201).json('You sent a POST request to create a new review');
+});
+
+// DELETE /api/courses/:courseId/reviews/:id 204 - Deletes the specified review and returns no content
+router.delete('/courses/:courseId/reviews/:id', function (req, res, next) {
+  res.status(204).json('You sent a DELETE request to delete a review: ' + req.params.id);
+});
+
+module.exports = router;

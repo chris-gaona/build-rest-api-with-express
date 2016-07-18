1d8dc75428c159f5b1d28e2a9d4b4a9e5f445465
Chris Gaona <thebeloved88@sbcglobal.net>
2016-07-17 20:08:40 -0700

    Updates the User model to store the user's password as a hashed value

--- src/models/users.js
+++ src/models/users.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var saltRounds = 10;
 
 // User
 // _id (ObjectId, auto-generated)
@@ -9,12 +11,30 @@ var mongoose = require('mongoose');
 // hashedPassword (String)
 
 var UserSchema = new mongoose.Schema({
-  fullName: String,
+  fullName: {
+    type: String,
+    required: true
+  },
   emailAddress: {
     type: String,
-    unique: true
+    unique: true,
+    required: true
   },
-  hashedPassword: String
+  hashedPassword: {
+    type: String,
+    required: true
+  }
+});
+
+UserSchema.pre('save', function (next) {
+  var user = this;
+  // Update the User model to store the user's password as a hashed value.
+  bcrypt.hash(user.hashedPassword, saltRounds, function(err, hash) {
+    if(err) return next(err);
+    // Store hash in your password DB.
+    user.hashedPassword = hash;
+    next();
+  });
 });
 
 var User = mongoose.model('User', UserSchema);
